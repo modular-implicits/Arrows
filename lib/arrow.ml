23bd0ff664e814ca8c95376acb602770584d6376
Daniel Vlasits <dvlasits@gmail.com>
2023-08-22 22:13:53 +0100

    some more arrows

--- lib/arrow.ml
+++ lib/arrow.ml
@@ -20,8 +20,21 @@ let second {A : Arrow} f = A.(arr swap >>> first f >>> arr swap)
 let ( *** ) {A : Arrow} f g = A.(first f >>> second g)
 let ( &&& ) {A : Arrow} f g = A.(arr (fun x -> (x, x)) >>> f *** g)
 
+module Kleisli (M : Monad) = struct 
+  type ('a, 'b) t = 'a -> 'b M.t 
+end 
 
-type ('a, 'b) kleisli = {M : Monad} -> 'a -> 'b M.t
+
+implicit module MonadArrow {M : Monad} : Arrow with type ('a, 'b) t = ('a, 'b) Kleisli(M).t 
+  = struct 
+    type ('a, 'b) t = ('a, 'b) Kleisli(M).t
+
+    let arr f = fun x -> M.return (f x)
+
+    let ( >>> ) f g = fun x -> M.bind (f x) g
+    let first f = fun (x, y) -> M.bind (f x) (fun x' -> M.return (x', y))
+end
+
 
 type ('a, 'b) either = Left of 'a | Right of 'b
 
@@ -30,18 +43,24 @@ module type ArrowApply = sig
   val app : (((('b, 'c) t) * 'b), 'c) t
 end
 
+let app {A : ArrowApply} = A.app
+
 module type ArrowChoice = sig 
   include Arrow
   val left : ('b, 'c) t -> (('b, 'd) either, ('c, 'd) either) t
 end
 
-implicit module MonadArrow {M : Monad} : Arrow with type ('a, 'b) t = ('a, 'b) kleisli = struct 
-  type ('a, 'b) t = ('a, 'b) kleisli
+let left {A : ArrowChoice} = A.left
 
-  let arr f = fun x -> M.return (f x)
+let right {A : ArrowChoice} f = let mirror = fun x -> match x with Left x -> Right x | Right x -> Left x in
+  A.(arr mirror >>> left f >>> arr mirror)
 
-  let ( >>> ) f g = fun x -> M.bind (f x) g
-  let first f = fun (x, y) -> M.bind (f x) (fun x' -> M.return (x', y))
-end
+let ( +++ ) {A : ArrowChoice} f g = A.(left f >>> right g)
+
+let ( ||| ) {A : ArrowChoice} f g = let untag = fun x -> match x with Left x -> x | Right x -> x in
+                                          A.(arr untag >>> f +++ g)
+
+let addA {A : Arrow} f g = A.(f &&& g >>> arr (fun (x, y) -> x + y))
+
 
 
---
+++ tests/test.ml
@@ -0,0 +1,1 @@
+open Arrows.Arrow;;
